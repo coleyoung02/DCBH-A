0ddaf933b97d83ef61e779df262a6ae7e1d2115b
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2024-11-17 00:46:24 -0800

    Added basic room changing

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Door linkedDoor;
+    [SerializeField] private GameObject attachedRoom;
+    [SerializeField] private GameObject exitLocation;
+    private RoomManager roomManager;
+
+    void Start()
+    {
+        roomManager = FindObjectOfType<RoomManager>();
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+
+        roomManager.ChangeRoom(attachedRoom, linkedDoor.attachedRoom, linkedDoor, exitLocation);
+    }
+
+}
