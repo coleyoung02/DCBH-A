6f8fd5eeb6aa01a9b5665966e67ac9f98f1dc84f
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2024-11-23 15:06:53 -0800

    Added shooting animations

--- Assets/Scripts/bullet_spawn_script.cs
+++ Assets/Scripts/bullet_spawn_script.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class bullet_spawn_script : MonoBehaviour
 {
     public GameObject bullet;
+    private Animator animator;
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
@@ -23,6 +29,9 @@ public class bullet_spawn_script : MonoBehaviour
             Vector3 direction = (targetPosition - transform.position).normalized;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             Quaternion rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+            animator.SetFloat("MouseX", direction.x);
+            animator.SetTrigger("Shoot");
 
             GameObject newBullet = Instantiate(bullet, transform.position, rotation);
             newBullet.GetComponent<Bullet>().SetDirection(direction);
